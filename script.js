5e9fa89aec8df0795ae43713d5556807a1155832
Kayla Jones <kayla3445@yahoo.com>
2022-04-02 18:49:31 -0500

    working on touch

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ const arrow = document.querySelector(".arrow");
 const error = document.querySelector(".error-text");
 const form = document.querySelector(".form");
 const email = document.querySelector(".email-input");
+const navItems = [...document.querySelectorAll(".nav-item")];
 
-function addClass() {
+function addActiveArrow() {
   arrow.classList.add("arrow-active");
 }
-function removeClass() {
+function removeActiveArrow() {
   arrow.classList.remove("arrow-active");
 }
 
-worksContainer.addEventListener("mouseover", addClass);
-worksContainer.addEventListener("mouseout", removeClass);
+worksContainer.addEventListener("mouseover", addActiveArrow);
+worksContainer.addEventListener("mouseout", removeActiveArrow);
 
-worksContainer.addEventListener("touchstart", addClass);
-worksContainer.addEventListener("touchend", removeClass);
+worksContainer.addEventListener("touchstart", addActiveArrow);
+worksContainer.addEventListener("touchend", removeActiveArrow);
 
 function validateForm(e) {
   const emailValue = email.value;
@@ -27,3 +28,17 @@ function validateForm(e) {
 }
 
 form.addEventListener("keyup", validateForm);
+
+function addActiveNav(e) {
+  const navItem = e.currentTarget;
+  console.log(navItem);
+  navItem.classList.add("nav-item-active");
+}
+
+function addRemoveNav(e) {
+  const navItem = e.currentTarget;
+  navItem.classList.remove("nav-item-active");
+}
+
+navItems.forEach((el) => el.addEventListener("touchstart", addActiveNav));
+navItems.forEach((el) => el.addEventListener("touchstart", addRemoveNav));
