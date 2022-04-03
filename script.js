a4b0432dd4a8ccb3b7214d324b569ee528d77874
Kayla Jones <kayla3445@yahoo.com>
2022-04-02 19:14:49 -0500

    done

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const error = document.querySelector(".error-text");
 const form = document.querySelector(".form");
 const email = document.querySelector(".email-input");
 const navItems = [...document.querySelectorAll(".nav-item")];
+const ctaBtn = document.querySelector(".get-started");
+const emailBtn = document.querySelector(".email-btn");
+const btns = [ctaBtn, emailBtn];
+const navFooter = [...document.querySelectorAll(".nav-item-footer")];
+const social = [...document.querySelectorAll(".social-img")];
 
 function addActiveArrow() {
   arrow.classList.add("arrow-active");
@@ -31,14 +36,68 @@ form.addEventListener("keyup", validateForm);
 
 function addActiveNav(e) {
   const navItem = e.currentTarget;
-  console.log(navItem);
   navItem.classList.add("nav-item-active");
 }
 
-function addRemoveNav(e) {
+function removeActiveNav(e) {
   const navItem = e.currentTarget;
   navItem.classList.remove("nav-item-active");
 }
 
 navItems.forEach((el) => el.addEventListener("touchstart", addActiveNav));
-navItems.forEach((el) => el.addEventListener("touchstart", addRemoveNav));
+navItems.forEach((el) => el.addEventListener("touchend", removeActiveNav));
+
+function addActiveCtaBtn(e) {
+  const btn = e.currentTarget;
+  btn.classList.add("active-btn");
+}
+
+function removeActiveCtaBtn(e) {
+  const btn = e.currentTarget;
+  btn.classList.remove("active-btn");
+}
+
+btns.forEach((el) => el.addEventListener("touchstart", addActiveCtaBtn));
+btns.forEach((el) => el.addEventListener("touchend", removeActiveCtaBtn));
+
+function addActiveNavFooter(e) {
+  const navItem = e.currentTarget;
+  navItem.classList.add("nav-item-footer-active");
+}
+
+function removeActiveNavFooter(e) {
+  const navItem = e.currentTarget;
+  navItem.classList.remove("nav-item-footer-active");
+}
+
+navFooter.forEach((el) =>
+  el.addEventListener("touchstart", addActiveNavFooter)
+);
+navFooter.forEach((el) =>
+  el.addEventListener("touchend", removeActiveNavFooter)
+);
+
+function addActiveSocial(e) {
+  const social = e.currentTarget;
+  if (social.classList.contains("facebook")) {
+    social.classList.add("facebook-active");
+  } else if (social.classList.contains("instagram")) {
+    social.classList.add("instagram-active");
+  } else {
+    social.classList.add("twitter-active");
+  }
+}
+
+function removeActiveSocial(e) {
+  const social = e.currentTarget;
+  if (social.classList.contains("facebook")) {
+    social.classList.remove("facebook-active");
+  } else if (social.classList.contains("instagram")) {
+    social.classList.remove("instagram-active");
+  } else {
+    social.classList.remove("twitter-active");
+  }
+}
+
+social.forEach((el) => el.addEventListener("touchstart", addActiveSocial));
+social.forEach((el) => el.addEventListener("touchend", removeActiveSocial));
